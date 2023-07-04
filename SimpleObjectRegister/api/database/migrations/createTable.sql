451cc71fe5082a351bb19632f5fae83cdaf48859
Thiago Henrique dos Santos <72514176+Thiago-Henrique-Santos@users.noreply.github.com>
2023-07-04 18:17:08 -0300

    Ajustes no arquivo da pasta migration do banco de dados

---
+++ SimpleObjectRegister/api/database/migrations/createTable.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS SimpleObjectRegister;
+
+CREATE TABLE IF NOT EXISTS person (
+    fisrtname TEXT, 
+    lastname TEXT, 
+    height REAL,
+    weight REAL,
+    birthdate DATE
+);
