b9ff95be440d8f7df64f3fb272711ff272a14260
Thiago Henrique dos Santos <72514176+Thiago-Henrique-Santos@users.noreply.github.com>
2023-12-20 10:51:10 -0300

    perf: :zap: Habilitando acessos

--- SimpleObjectRegister/api/server.js
+++ SimpleObjectRegister/api/server.js
@@ -5,6 +5,10 @@ const hostname = '127.0.0.1';
 const port = 3333;
 
 const server = http.createServer((req, res)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     const url = req.url;
 
     if (req.method == 'GET') {
